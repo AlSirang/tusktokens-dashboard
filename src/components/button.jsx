77f2af01681311512917290e289fd4598359d7e9
Bashir Uddin <uddinbashir1999@gmail.com>
2024-06-04 14:27:14 +0500

    fix: update theme colors

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -9,9 +9,9 @@ export const Button = forwardRef(
         className={cn(
           "inline-flex justify-center items-center py-3 px-4 rounded-md w-full font-medium uppercase transition-all disabled:text-foreground/50 disabled:bg-primary/30 disabled:opacity-80 hover:opacity-80",
           {
-            "text-dark-100 bg-yellow-200 disabled:bg-yellow-200/60":
+            "text-white bg-yellow-200 disabled:bg-yellow-200/60":
               variant === "primary",
-            "text-foreground bg-transparent border border-yellow-200 disabled:bg-transparent disabled:border-yellow-200/50":
+            "text-white bg-transparent border border-yellow-200 disabled:bg-transparent disabled:border-yellow-200/50":
               variant === "secondary",
           },
           className
--- src/components/card/index.jsx
+++ src/components/card/index.jsx
@@ -4,7 +4,7 @@ export const Card = ({ className = "", children }) => {
   return (
     <section
       className={cn(
-        "bg-dark-200 bg-gradient-to-b from-[#151920] to-[#282828] shadow-[0px_0px_5px_-3px] rounded-[10px] h-full w-full inset-1",
+        "bg-dark-200 bg-gradient-to-b from-dark-100 to-dark-200 shadow-[0px_0px_5px_-3px] rounded-[10px] h-full w-full inset-1",
         className
       )}
     >
